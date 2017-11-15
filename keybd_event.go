d323d1c2bff24d3482f9e80517baf21d3aaa9eac
micmonay <micmonay@gmail.com>
2017-11-15 09:10:28 +0100

    No change functional, just gofmt

--- keybd_event.go
+++ keybd_event.go
@@ -1,42 +1,45 @@
-/*
-	This package is used for a key press simulated in Windows, Linux and Mac
-*/
+// Package keybd_event is used for a key press simulated in Windows, Linux and Mac
 package keybd_event
+
 type KeyBonding struct {
-	hasCTRL bool
-	hasALT bool
-	hasSHIFT bool
-	hasRCTRL bool
+	hasCTRL   bool
+	hasALT    bool
+	hasSHIFT  bool
+	hasRCTRL  bool
 	hasRSHIFT bool
-	keys[] int
+	keys      []int
 }
+
 //Use for create struct KeyBounding
-func NewKeyBonding() (KeyBonding,error){
+func NewKeyBonding() (KeyBonding, error) {
 	keyBounding := KeyBonding{}
-	keyBounding.hasALT=false
-	keyBounding.hasCTRL=false
-	keyBounding.hasSHIFT=false
-	keyBounding.hasRCTRL=false
-	keyBounding.hasRSHIFT=false
+	keyBounding.hasALT = false
+	keyBounding.hasCTRL = false
+	keyBounding.hasSHIFT = false
+	keyBounding.hasRCTRL = false
+	keyBounding.hasRSHIFT = false
 	keyBounding.keys = []int{}
 	err := initKeyBD()
-	if err!=nil {
-		return keyBounding,err
+	if err != nil {
+		return keyBounding, err
 	}
-	return keyBounding,nil
+	return keyBounding, nil
 }
-func (k *KeyBonding) SetKeys(keys... int){
+func (k *KeyBonding) SetKeys(keys ...int) {
 	k.keys = keys
 }
+
 //If key ALT pressed
 func (k *KeyBonding) HasALT(b bool) {
-	k.hasALT=b
+	k.hasALT = b
 }
+
 //If key CTRL pressed
 func (k *KeyBonding) HasCTRL(b bool) {
-	k.hasCTRL=b
+	k.hasCTRL = b
 }
+
 //If key SHIFT pressed
 func (k *KeyBonding) HasSHIFT(b bool) {
-	k.hasSHIFT=b
+	k.hasSHIFT = b
 }
--- keybd_event_test.go
+++ keybd_event_test.go
@@ -1,25 +1,25 @@
 package keybd_event
 
 import (
-	"time"
 	"runtime"
+	"time"
 )
 
-func ExampleNewKeyBonding(){
-	kb,err := NewKeyBonding()
-	if err!=nil {
+func ExampleNewKeyBonding() {
+	kb, err := NewKeyBonding()
+	if err != nil {
 		panic(err)
 	}
 	// For linux, it is very important wait 2 seconds
 	if runtime.GOOS == "linux" {
 		time.Sleep(2 * time.Second)
 	}
-	kb.SetKeys(VK_A,VK_B) //set keys
+	kb.SetKeys(VK_A, VK_B) //set keys
 
 	kb.HasSHIFT(true) //set shif is pressed
 
 	err = kb.Launching() //launch
-	if err!=nil {
+	if err != nil {
 		panic(err)
 	}
 	// Output: AB
